95c11f20055182703856850a0016c8f60054dee9
otavio_dm <102213097+magnaniotavio@users.noreply.github.com>
2023-03-15 09:47:17 -0400

    Added some more comments.

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+/* In this class we set the entry as the text which is writen in search-bar form 
+at the moment we make the search. Once the entry is set, the code in the App.js file
+will be able to operate on it. */
+
 class SearchInput extends React.Component {
   
   constructor(props){
@@ -21,7 +25,9 @@ render() {
             className="search-bar"
             type="text"
             placeholder='search...'
+            // Handles the changes by setting the state
             onChange={(event) => this.setState({entry:event.target.value})}
+            // Sets the entry as the value of the form
             value={this.state.entry}           />
       </form>
     </div>
